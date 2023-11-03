f4f6c188d5010806f2f325cec3a995452b358799
wfceza <73784410+wfceza@users.noreply.github.com>
2023-11-03 06:16:21 -0700

    Update Hotproduct.js

--- data/Hotproduct.js
+++ data/Hotproduct.js
@@ -1,10 +1,10 @@
 const hotDeals = [
     {
         id: "1c079479-8586-494f-ab53-219325432536",
-        image: "images/products/men-athletic-shoes-green.jpg",
+        image: "./products/men-athletic-shoes-green.jpg",
         name: "Men's Athletic Sneaker",
         rating: {
-          stars: 'images/ratings/rating-40.png',
+          stars: './ratings/rating-40.png',
           count: 229
         },
         priceCents: '3,890',
@@ -17,10 +17,10 @@ const hotDeals = [
       },
       {
         id: "d2785924-743d-49b3-8f03-ec258e640503",
-        image: "images/products/women-chunky-beanie-gray.webp",
+        image: "./products/women-chunky-beanie-gray.webp",
         name: "Women's Chunky Cable Beanie - Gray",
         rating: {
-          stars: 'images/ratings/rating-50.png',
+          stars: './ratings/rating-50.png',
           count: 83
         },
         priceCents: '2,500',
@@ -35,10 +35,10 @@ const hotDeals = [
       },
       {
         id: "54e0eccd-8f36-462b-b68a-8182611d9add",
-        image: "images/products/black-2-slot-toaster.jpg",
+        image: "./products/black-2-slot-toaster.jpg",
         name: "2 Slot Toaster - Black",
         rating: {
-          stars: 'images/ratings/rating-50.png',
+          stars: './ratings/rating-50.png',
           count: 2197
         },
         priceCents: '11,000',
@@ -50,10 +50,10 @@ const hotDeals = [
       },
       {
         id: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
-        image: "images/products/vanity-mirror-silver.jpg",
+        image: "./products/vanity-mirror-silver.jpg",
         name: "Vanity Mirror with Heavy Base - Chrome",
         rating: {
-          stars: 'images/ratings/rating-45.png',
+          stars: './ratings/rating-45.png',
           count: 130
         },
         priceCents: '2,400',
